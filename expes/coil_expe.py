0b48c47484d913f91b576bd616ae039a1decf16d
Titouan Vayer <titouan.vayer@gmail.com>
2023-10-10 15:24:17 +0200

    coil experience

--- expes/coil_expe.py
+++ expes/coil_expe.py
@@ -1,6 +1,8 @@
 
 # %%
-from sklearn.manifold import TSNE  # comparison with scikit-learn implementation
+# comparison with scikit-learn implementation
+import pickle
+from sklearn.manifold import TSNE
 import matplotlib.pyplot as plt
 from snekhorn.utils import COIL_dataset
 import torch
@@ -8,6 +10,7 @@ from snekhorn.utils import PCA
 from snekhorn.affinities import SymmetricEntropicAffinity, NanError
 from snekhorn.dimension_reduction import DRWrapper
 from sklearn import metrics
+from sklearn.manifold import trustworthiness
 
 torch.manual_seed(16)
 # %% Load the COIL dataset
@@ -18,9 +21,9 @@ pca = PCA(n_components=50)
 # we make a preprocessing with PCA in dim 50
 X_process = pca.fit_transform(X_coil)
 # %%
-perps_test = [10, 30, 50, 100, 200, 400, 500, 800, 1000]
+perps_test = [5, 10, 30, 50, 100, 200, 400, 500, 800, 1000]
 all_affinities = {}
-max_iter_aff = 3000
+max_iter_aff = 4500
 # precompute affinities, try two learning rates
 for i, perp in enumerate(perps_test):
     try:
@@ -29,18 +32,17 @@ for i, perp in enumerate(perps_test):
         PX = symmetric_entropic_affinity.compute_affinity(X_process)
     except NanError as e:
         symmetric_entropic_affinity = SymmetricEntropicAffinity(
-            perp=perp, lr=0.5, max_iter=max_iter_aff, square_parametrization=False)
+            perp=perp, lr=0.2, max_iter=max_iter_aff, square_parametrization=False)
         PX = symmetric_entropic_affinity.compute_affinity(X_process)
     all_affinities[perp] = PX
     print('Perp = {} done'.format(perp))
 # %% Save the affinities
-# import pickle
-# with open('../data/saved/symmetric_entropic_affinities_COIL.pkl', 'wb') as f:
-#     pickle.dump(all_affinities, f)
+with open('../data/saved/symmetric_entropic_affinities_COIL.pkl', 'wb') as f:
+    pickle.dump(all_affinities, f)
 # %%
 lr = 1.0
 max_iter = 1500
-max_iter_sinkhorn = 20
+max_iter_sinkhorn = 5
 tsnekhorn = DRWrapper(affinity_data='precomputed',
                       affinity_embedding='student_bistochastic',
                       params_affinity_embedding={
@@ -54,8 +56,10 @@ simpletsnekhorn = DRWrapper(affinity_data='precomputed',
                             affinity_embedding='student',
                             lr=lr,
                             max_iter=max_iter)
-res = {}
-for model, name in zip([tsnekhorn, simpletsnekhorn, None], ['tSNEkhorn', 'Simple tSNEkhorn', 'tSNE (sklearn)']):
+silhouette_scores = {}
+trustworthiness_scores = {}
+for model, name in zip([simpletsnekhorn, tsnekhorn, None],
+                       ['Simple tSNEkhorn', 'tSNEkhorn', 'tSNE (sklearn)']):
     for i, perp in enumerate(perps_test):
         if name == 'tSNE (sklearn)':
             tsne_sklearn = TSNE(perplexity=perp)
@@ -65,23 +69,36 @@ for model, name in zip([tsnekhorn, simpletsnekhorn, None], ['tSNEkhorn', 'Simple
             PX_ = all_affinities[perp]
             model.fit(PX_)
             emb = model.embedding_
-        score = float(metrics.silhouette_score(emb, Y_coil))
-        if name not in res.keys():
-            res[name] = [score]
-        else:
-            res[name].append(score)
+        # calculate scores and store them
+        silhouette = float(metrics.silhouette_score(emb, Y_coil))
+        trust_score = float(trustworthiness(X_process, emb))
+        for score, dic_score in zip([silhouette, trust_score],
+                                    [silhouette_scores, trustworthiness_scores]):
+            if name not in dic_score.keys():
+                dic_score[name] = [score]
+            else:
+                dic_score[name].append(score)
+
     print('!!!! Model {} done... !!!!'.format(name))
 
 # %%
 cmap = plt.cm.get_cmap('tab10')
 fs = 15
-fig, ax = plt.subplots(1, 1, figsize=(5, 5))
-for i, name in enumerate(['Simple tSNEkhorn', 'tSNEkhorn', 'tSNE (sklearn)']):
-    ax.plot(perps_test, res[name], marker='o', c=cmap(i), lw=2, label=name)
-ax.legend(fontsize=fs-2)
-ax.grid()
-ax.set_xscale('log')
-ax.set_xlabel('Perplexity', fontsize=fs)
-ax.set_ylabel('Silhouette score', fontsize=fs)
-ax.set_title('Silhouette scores on COIL dataset', fontsize=fs+2)
+fig, ax = plt.subplots(1, 2, figsize=(10, 5))
+j = 0
+for name_score, dic_score in zip(['Silhouette', 'Trustworthiness'],
+                                 [silhouette_scores, trustworthiness_scores]):
+    for i, name in enumerate(['Simple tSNEkhorn', 'tSNEkhorn', 'tSNE (sklearn)']):
+        ax[j].plot(perps_test, dic_score[name],
+                   marker='o', c=cmap(i), lw=2, label=name)
+    ax[j].legend(fontsize=fs-2)
+    ax[j].grid()
+    ax[j].set_xscale('log')
+    ax[j].set_xlabel('Perplexity', fontsize=fs)
+    ax[j].set_ylabel('{} score'.format(name_score), fontsize=fs)
+    j += 1
+plt.title(
+    'Silhouette/ Trustworthiness scores on COIL', fontsize=fs)
+plt.tight_layout()
+
 # %%
